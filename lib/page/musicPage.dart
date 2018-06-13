4850c01f0515183b3e12f588ef1effa0ff494e20
talwl <talwl@163.com>
2018-06-13 18:09:42 +0800

    1.音乐页面完成，请指示

--- lib/page/musicPage.dart
+++ lib/page/musicPage.dart
@@ -13,7 +13,7 @@ class MusicPage extends StatefulWidget {
   MusicList data;
 
   @override
-  _MusicPageState createState() => new _MusicPageState();
+  _MusicPageState createState() => _MusicPageState();
 }
 
 class _MusicPageState extends State<MusicPage> {
@@ -28,7 +28,7 @@ class _MusicPageState extends State<MusicPage> {
           });
         },
         onSuccess: (result) {
-          MusicList musicList = new MusicList.fromHtml(result);
+          MusicList musicList = MusicList.fromHtml(result);
           setState(() {
             widget.data = musicList;
           });
@@ -74,16 +74,16 @@ class _MusicPageState extends State<MusicPage> {
   //body
   _body() {
     _initController();
-    return new NestedScrollView(
+    return NestedScrollView(
       headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
         return <Widget>[
-          new SliverOverlapAbsorber(
+          SliverOverlapAbsorber(
             handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
             child: _getTopWidgets(),
           ),
         ];
       },
-      body: new Builder(
+      body: Builder(
         builder: (BuildContext context) {
           return _getCenterWidgets(context);
         },
@@ -105,42 +105,55 @@ class _MusicPageState extends State<MusicPage> {
     );
   }
 
-  //滑动部分
+  //轮播图下面滑动部分
   _getCenterWidgets(BuildContext context) {
     return new CustomScrollView(
-      slivers: <Widget>[
-        new SliverOverlapInjector(
-          handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-        ),
-        SliverMusicTitle(widget.data.m250List.title),
-        SliverList(
-          delegate: new SliverChildListDelegate([_get250Widget(),]),
-        ),
-        SliverMusicTitle(widget.data.editList.title),
-        SliverList(
-          delegate: new SliverChildListDelegate([_getEditWidget(),]),
-        ),
-      ],
+      slivers: _getCustomBody(context),
     );
   }
-  //获取顶部
+
+  //滑动部分的widget集合
+  _getCustomBody(BuildContext context) {
+    List<Widget> widgets = <Widget>[
+      new SliverOverlapInjector(
+        handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+      ),
+      SliverTitle(widget.data.m250List.title),
+      SliverList(
+        delegate: SliverChildListDelegate([
+          _get250Widget(),
+        ]),
+      ),
+      SliverTitle(widget.data.editList.title),
+      SliverList(
+        delegate: SliverChildListDelegate([
+          _getEditWidget(),
+        ]),
+      ),
+    ];
+    widgets.addAll(_getFashionWidget());
+
+    return widgets;
+  }
+
+  //豆瓣250
   _get250Widget() {
     List<Widget> m250List =
         new List.generate(widget.data.m250List.itemList.length, (index) {
       Music250Item music250item = widget.data.m250List.itemList[index];
-      return new Padding(
+      return Padding(
         padding: const EdgeInsets.all(4.0),
-        child: new Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new ClipOval(
+            ClipOval(
               child: new Image.network(
                 music250item.imageAddress,
                 width: 70.0,
                 height: 70.0,
               ),
             ),
-            new Container(
+            Container(
               child: Text(music250item.title),
               alignment: Alignment.center,
               padding: const EdgeInsets.symmetric(vertical: 8.0),
@@ -149,49 +162,18 @@ class _MusicPageState extends State<MusicPage> {
         ),
       );
     });
-    return new SingleChildScrollView(
+    return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
-      child: new Row(
+      child: Row(
         children: m250List,
       ),
     );
   }
 
-//   _getScrollerWidget(){
-//    List<Widget> widgets=;
-////    widgets.addAll(_getFashionWidget());
-//    return widgets;
-//  }
-
-  _getFashionWidget(){
-    List<Widget> widgets=[];
-    widget.data.fashionList.forEach((e){
-        widgets.add(SliverMusicTitle(e.title));
-        widgets.add(SliverGrid(
-          delegate: SliverChildBuilderDelegate(
-                (
-                BuildContext context,
-                index,
-                ) {
-              return Text(e.itemList[index].name);
-            },
-            childCount: e.itemList.length,
-          ),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 5.0,
-            mainAxisSpacing: 5.0,
-            childAspectRatio: 0.7,
-          ),
-        ));
-    });
-    return widgets;
-  }
-
   //编辑推荐
   _getEditWidget() {
     List<Widget> editList =
-    new List.generate(widget.data.editList.itemList.length, (index) {
+        new List.generate(widget.data.editList.itemList.length, (index) {
       MusicEditItem musicedititem = widget.data.editList.itemList[index];
       return new Padding(
         padding: const EdgeInsets.all(4.0),
@@ -202,19 +184,28 @@ class _MusicPageState extends State<MusicPage> {
               new Image.network(
                 musicedititem.imageAddress,
               ),
-               Container(
-                child: Text(musicedititem.name,style: Theme.of(context).textTheme.title,),
+              Container(
+                child: Text(
+                  musicedititem.name,
+                  style: Theme.of(context).textTheme.title,
+                ),
                 alignment: Alignment.center,
                 padding: const EdgeInsets.symmetric(vertical: 2.0),
               ),
               Container(
-                child: Text(musicedititem.des,style: Theme.of(context).textTheme.body2,),
+                child: Text(
+                  musicedititem.des,
+                  style: Theme.of(context).textTheme.body2,
+                ),
                 alignment: Alignment.center,
               ),
               Container(
                 width: 150.0,
-                padding: const EdgeInsets.fromLTRB(4.0,8.0,4.0,8.0),
-                child: Text(musicedititem.summery,style: Theme.of(context).textTheme.body2,),
+                padding: const EdgeInsets.fromLTRB(4.0, 8.0, 4.0, 8.0),
+                child: Text(
+                  musicedititem.summery,
+                  style: Theme.of(context).textTheme.body2,
+                ),
                 alignment: Alignment.center,
               ),
             ],
@@ -230,37 +221,125 @@ class _MusicPageState extends State<MusicPage> {
       ),
     );
   }
+
+  //本周流行
+  _getFashionWidget() {
+    List<Widget> widgets = [];
+    widget.data.fashionList.forEach((e) {
+      widgets.add(SliverTitle(e.title));
+      widgets.add(SliverGrid(
+        delegate: SliverChildBuilderDelegate(
+          (
+            BuildContext context,
+            index,
+          ) {
+            return new Card(
+              child: Container(
+                  alignment: Alignment.center,
+                  child: Stack(
+                    children: <Widget>[
+                      new Image.network(
+                        e.itemList[index].imageAddress,
+                        fit: BoxFit.cover,
+                        height: 200.0,
+                        width: 200.0,
+                      ),
+                      new Container(
+                        padding: const EdgeInsets.all(8.0),
+                        alignment: Alignment.bottomCenter,
+                        child: new Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            Text(
+                              e.itemList[index].name,
+                              style: Theme
+                                  .of(context)
+                                  .textTheme
+                                  .body2
+                                  .copyWith(color: Colors.white),
+                            ),
+                            Text(
+                              e.itemList[index].type,
+                              style: Theme
+                                  .of(context)
+                                  .textTheme
+                                  .body1
+                                  .copyWith(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                        decoration: new BoxDecoration(
+                          gradient: new LinearGradient(
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            colors: [
+                              Colors.black,
+                              Colors.black54,
+                              Colors.black12,
+                              Colors.transparent,
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+            );
+          },
+          childCount: e.itemList.length,
+        ),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 5.0,
+          mainAxisSpacing: 5.0,
+          childAspectRatio: 1.0,
+        ),
+      ));
+    });
+    return widgets;
+  }
 }
 
-class SliverMusicTitle extends StatelessWidget {
-  SliverMusicTitle(this.title);
+//Sliver的标题头部
+class SliverTitle extends StatelessWidget {
+  SliverTitle(this.title);
+
   final String title;
 
   @override
   Widget build(BuildContext context) {
     return SliverPersistentHeader(
-      delegate: SliverTitle(title: title),
+      delegate: SliverTitleDelegate(title: title),
     );
   }
 }
-class SliverTitle extends SliverPersistentHeaderDelegate{
 
-  SliverTitle({@required this.title, this.height});
+//delegate标题头部
+class SliverTitleDelegate extends SliverPersistentHeaderDelegate {
+  SliverTitleDelegate({@required this.title, this.height});
+
   final String title;
   final double height;
   BuildContext context;
 
   @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    this.context=context;
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    this.context = context;
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      alignment: Alignment.centerLeft,
-        child: Text(
-          title ?? '标题',
-          style: Theme.of(context).textTheme.title.copyWith(color: Theme.of(context).primaryColor),
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        alignment: Alignment.centerLeft,
+        child: new Opacity(
+          opacity: shrinkOffset > minExtent
+              ? (maxExtent - shrinkOffset) / maxExtent
+              : 1.0,
+          child: Text(
+            title ?? '标题',
+            style: Theme.of(context).textTheme.title.copyWith(
+                color: Theme.of(context).primaryColor, fontFamily: 'Merri'),
+          ),
         ));
   }
+
   @override
   double get maxExtent => height ?? 60.0;
 
@@ -271,24 +350,28 @@ class SliverTitle extends SliverPersistentHeaderDelegate{
   bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
     return true;
   }
-
 }
+
+//sliver轮播图
 class SliverBanner extends SliverPersistentHeaderDelegate {
   SliverBanner({@required this.childs});
 
   final List<Widget> childs;
-  int position;
+  int position = 0;
 
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return PageView.builder(
-      onPageChanged: _setPosition,
-      scrollDirection: Axis.vertical,
-      itemBuilder: (build, index) {
-        return childs[index];
-      },
-      itemCount: childs.length,
+    return new Opacity(
+      opacity: (maxExtent - shrinkOffset) / maxExtent,
+      child: PageView.builder(
+        onPageChanged: _setPosition,
+        scrollDirection: Axis.vertical,
+        itemBuilder: (build, index) {
+          return childs[index];
+        },
+        itemCount: childs.length,
+      ),
     );
   }
 
@@ -305,5 +388,31 @@ class SliverBanner extends SliverPersistentHeaderDelegate {
   @override
   bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
     return true;
+  }
+}
+
+class Indication extends StatelessWidget {
+  Indication(this.size, this.position);
+
+  final int size;
+  final int position;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: new List<Widget>.generate(size, (index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
+          child: Icon(
+            Icons.brightness_1,
+            size: 10.0,
+            color: position == index ? Colors.grey : Colors.white,
+          ),
+        );
+      }),
+    );
   }
 }
